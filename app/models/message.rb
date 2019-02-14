adaec398fce2c79e3cdd2c2c4a08054c09388c9f
Dove <saharak@bananacoding.com>
2019-02-14 10:36:16 +0700

    [Fixed] code

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+  belongs_to :message_as_sender, foreign_key: "sender_id", class_name: "User"
+  belongs_to :message_as_recipient, foreign_key: "recipient_id", class_name: "User"
 end
